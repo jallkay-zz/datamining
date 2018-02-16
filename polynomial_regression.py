144c89a23ad77509ce67aeb6d254bea73836777d
James Allington-Kay <lancaster.1@hotmail.co.uk>
2018-02-16 11:58:44 +0000

    Full beta test 1
    Needs more polishing and plotting

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -1,46 +1,38 @@
 import numpy as np
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
-
-
-plot_config=['bs', 'b*', 'g^']
-plt.plot(x_test, y_test, 'ro', label="Actual")
-# 3. Set the polynomial degree to be fitted betwee 1 and 3
-top_degree=3
-d_degree = np.arange(1,top_degree+1)
-for degree in d_degree:
-     
-    poly_fit = np.poly1d(np.polyfit(x_train,y_train, degree))
-    # print poly_fit.coeffs
-     
-    # 4. Fit the numpy polynomial
-    predict=poly_fit(x_test)
-    error=np.mean((predict-y_test)**2)
-     
-    print "Error with poly1d at degree %s is %s" %(degree, error)
-     
-    # 5. Create a fit a polynomial with sk-learn LinearRegression
-    model = Pipeline([('poly', PolynomialFeatures(degree=degree)),('linear', linear_model.LinearRegression())])
-    model=model.fit(x_train[:,np.newaxis], y_train[:,np.newaxis])
-     
-    predict_sk=model.predict(x_test[:,np.newaxis])
-    error_sk=np.mean((predict_sk.flatten()-y_test)**2)
-     
-    print "Error with sk-learn.LinearRegression at degree %s is %s" %(degree, error_sk)
-     
-    #plt.plot(x_test, y_test, '-', label="Actual")
-    plt.plot(x_test, predict, plot_config[degree-1], label="Fit "+str(degree)+ " degree poly")
-    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-               ncol=2, mode="expand", borderaxespad=0.)
- 
-plt.show()
-
-
+import math
 
 def poly_regression(x_train, y_train, degree):
-    np.arange(1,degree+1)
+    ''' Function for getting the polynomial coefficients for fitting the test data '''
+    degree = np.arange(1,degree+1)
     for iterDegree in degree:
         fit = np.poly1d(np.polyfit(x_train, y_train, iterDegree))
-        print fit.coeffs
     return fit
+
+def eval_poly_regression(parameters, x, y, degree):
+    ''' Function for predicting values based on the fitted coefficients '''
+    predict = parameters(x)
+    error = math.sqrt(np.mean((predict-y)**2))
+    print "Error with polynomial regression at degree %s is %s" %(degree, error)
+
+df = pd.read_csv('regression_train_assignment2017.csv', sep=',')
+
+trainSlice = int(round(df.count()[0] * 0.70))
+testSlice = int(df.count()[0] - round(df.count()[0] * 0.30))
+
+
+x_train = df['x'][trainSlice:].as_matrix()
+y_train = df['y'][trainSlice:].as_matrix()
+
+x_test = df['x'][:testSlice].as_matrix()
+y_test = df['y'][:testSlice].as_matrix()
+
+fit = poly_regression(x_train, y_train, 5)
+
+print fit.coeffs
+
+eval_poly_regression(fit, x_test, y_test, 5)
+
+
 
